e16fa8ba886e6613d79b2098ae37689d4b65bce8
Emili084 <128594426+Emili084@users.noreply.github.com>
2023-06-14 16:01:53 +0200

    up

---
+++ fonction.js
@@ -0,0 +1,25 @@
+// 1. Renvoie une addition
+// 2. Renvoie une soustraction
+// 3. Renvoie une multiplication
+// 4. Renvoie un division
+// 5. Renvoie une chaîne de charactère qui dit je m'appelle name et j'ai age ans.
+
+const addition = (a, b) => {
+    return a + b;
+}
+
+const soustraction = (a, b) => {
+    return a - b;
+}
+
+const multiplication = (a, b) => {
+    return a * b;
+}
+
+const division = (a, b) => {
+    return a / b;
+}
+
+const presentation = (name, age) => {
+    return `Je m'appelle ${name} et j'ai ${age} ans.`;
+}
